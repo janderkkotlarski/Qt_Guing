62f3e54936f42818672c2e4e907bb2143271cad7
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-11-04 21:05:51 +0100

    added linux version .pro

--- Qt_Duing/main.cpp
+++ Qt_Duing/main.cpp
@@ -1,10 +1,11 @@
 #include <QApplication>
 #include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QLabel *label = new QLabel("Tadaima!");
+    QLabel *label = new QLabel("<h2><i>Tadaima</i> " "<font color=red>Choukai!</font></h2>");
     label->show();
     return a.exec();
 }
