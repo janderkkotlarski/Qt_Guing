4433b8edcd9f79cbde5c62d2326d13b2b8277e59
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-11-09 19:12:39 +0100

    Newer

--- Qt_Duing/main.cpp
+++ Qt_Duing/main.cpp
@@ -4,8 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    QLabel *label = new QLabel("<h2><i>Tadaima</i> " "<font color=red>Choukai!</font></h2>");
-    label->show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QPushButton *button = new QPushButton("Click to commit Sudoku!");
+
+    QObject::connect(button, SIGNAL(clicked()),
+                     &app, SLOT(quit()));
+
+    button->show();
+
+    // QLabel *label = new QLabel("<h2><i>Tadaima</i> " "<font color=red>Choukai!</font></h2>");
+    // label->show();
+
+    return app.exec();
 }
