ec3f292cd4c7a76be2d7d30bae76b705a63d0b34
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-11-24 15:54:43 +0100

    I'm nearly done with Qt, it's always some other bullshit, trying to learn something that requires even deeper knowledge and even deeper knowledge seemingly ad infinitum.

--- Cellalog/gotocelldialog.cpp
+++ Cellalog/gotocelldialog.cpp
@@ -5,7 +5,26 @@
 GoToCellDialog::GoToCellDialog(QWidget *parent)
     : QDialog(parent)
 {
-    setupUi(this);
+    setupUi(this);  //  WTF does this not work properly?
+
+    /*
+
+    D:\C++\Qt_Guing\Cellalog\gotocelldialog.cpp:-1: In constructor 'GoToCellDialog::GoToCellDialog(QWidget*)':
+
+    D:\C++\Qt_Guing\Cellalog\gotocelldialog.cpp:8: error: no matching function for call to 'GoToCellDialog::setupUi(GoToCellDialog*)'
+     setupUi(this);
+                 ^
+    D:\C++\Qt_Guing\Cellalog\gotocelldialog.h:6: In file included from ..\Cellalog\gotocelldialog.h:6:0,
+
+    D:\C++\Qt_Guing\Cellalog\gotocelldialog.cpp:3: from ..\Cellalog\gotocelldialog.cpp:3:
+
+    D:\C++\Qt_Guing\build-Cellalog-Desktop_Qt_6_1_0-Debug\ui_mainwindow.h:48: candidate: void Ui_GoToCellDialog::setupUi(QMainWindow*)
+     void setupUi(QMainWindow *GoToCellDialog)
+          ^~~~~~~
+
+    D:\C++\Qt_Guing\build-Cellalog-Desktop_Qt_6_1_0-Debug\ui_mainwindow.h:48: note:   no known conversion for argument 1 from 'GoToCellDialog*' to 'QMainWindow*'
+
+    */
 
     QRegExp regExp("[A-Za-z][1-9][0-9]{0, 2}");
     lineEdit->setValidator(new QRegExpValidator(regExp, this));
